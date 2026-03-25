da84f85c97f3c32ceac2b3a90373c890543841ff
Arthur Leandro <arthurfonseca263@gmail.com>
2026-03-25 09:13:31 -0300

    middleware e index.routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const livrosRoutes = require('./livros.routes');
+const usuariosRoutes = require('./usuarios.routes');
+const { logger } = require('../middlewares/main.middlewares')
 
+router.use('/livros', livrosRoutes)
 router.use('/usuarios', usuariosRoutes);
+router.use(logger)
 
 router.get('/', (req, res) => {
     res.json({ sistema: 'Biblioteca Arthur', status: 'Online' });
